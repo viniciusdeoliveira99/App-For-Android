9c53cf36ecf3c11133ce34a404ec776c6e7b659c
Vinícius Mattos <viniciusmattos99@gmail.com>
2020-03-17 16:58:46 -0300

    Actualización
    last - master

--- app/src/main/java/br/com/navegacao/MainActivity.java
+++ app/src/main/java/br/com/navegacao/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.navegacao;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +11,9 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,17 +31,22 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private AppBarConfiguration mAppBarConfiguration;
+    protected DrawerLayout drawerLayout;
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
 
-		//FAB
+        setupNavDrawer();
+
+        //FAB
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,16 +54,71 @@ public class MainActivity extends AppCompatActivity {
                 toast("FAB action");
             }
         });
-
-		//DRAWER LAYOUT
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
     }
+
+    protected void setupNavDrawer() {
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Ícone do menu do nav drawer
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_actual);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+            drawerLayout = findViewById(R.id.drawer_layout);
+            NavigationView navigationView = findViewById(R.id.nav_view);
+
+            if (navigationView != null && drawerLayout != null) {
+                navigationView.setNavigationItemSelectedListener(
+                        new NavigationView.OnNavigationItemSelectedListener() {
+                            @Override
+                            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                                menuItem.setChecked(true);
+                                closeDrawer();
+                                // Trata o evento do menu
+                                onNavDrawerItemSelected(menuItem);
+                                return true;
+                            }
+                        });
+            }
+        }
+    }
+
+
+    // Trata o evento do menu lateral
+    private void onNavDrawerItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                toast("Home");
+                break;
+            case R.id.nav_tools:
+                toast("Configurações");
+                break;
+            case R.id.nav_exit:
+                AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                adb.setTitle("Confirmação");
+                adb.setMessage("Deseja sair do sistema?");
+                adb.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(MainActivity.this, Login.class));
+                    }
+                });
+                adb.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                adb.show();
+                break;
+        }
+    }
+
 
     public void toast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,9 +129,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-      int id = item.getItemId();
-      if(id == R.id.action_about){
-      }
+        int id = item.getItemId();
+        if(id == R.id.action_about){
+            AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+            adb.setTitle("Sobre");
+            adb.setMessage("Sistema versão 1.0");
+            adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            adb.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public boolean onOpcaoSelecionada(MenuItem item){
+
+
+    // Abre o menu lateral
+    protected void openDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
+
+    // Fecha o menu lateral
+    protected void closeDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 
 }
